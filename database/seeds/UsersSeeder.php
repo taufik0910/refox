bc90cbe3bf4c34f5490f459399e8b474fc037ad5
taufik0910 <taufik.cradle@gmail.com>
2017-10-25 10:53:20 +0700

    refox

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::create([
+'name' => 'Admin',
+'email' => 'admin@gmail.com',
+'password' => bcrypt('secret'),
+'role' => 'admin'
+]);
+// sample customer
+App\User::create([
+'name' => 'customer',
+'email' => 'customer@gmail.com',
+'password' => bcrypt('secret'),
+'role' => 'customer'
+]);
+    }
+}
